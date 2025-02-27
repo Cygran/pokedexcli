9ff97a801ebf4631699ce314ca9596e117fff363
Cygran <apcanning90@gmail.com>
2025-02-27 17:23:37 +0000

    feat: integrate caching into PokeAPI client for location data retrieval

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -12,6 +12,16 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 		url = *pageURL
 	}
 
+	// Cache check
+	if cachedData, found := c.cache.Get(url); found {
+		locationsResp := RespShallowLocations{}
+		err := json.Unmarshal(cachedData, &locationsResp)
+		if err != nil {
+			return RespShallowLocations{}, err
+		}
+		return locationsResp, nil
+	}
+	//if not in cache HTTP request
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return RespShallowLocations{}, err
@@ -27,6 +37,8 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 	if err != nil {
 		return RespShallowLocations{}, err
 	}
+
+	c.cache.Add(url, dat)
 
 	locationsResp := RespShallowLocations{}
 	err = json.Unmarshal(dat, &locationsResp)
