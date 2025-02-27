ba5765a2a1cfc04f7da4b5ad23adddfd79e92f08
Cygran <apcanning90@gmail.com>
2025-02-27 14:17:17 +0000

    refactor: implement REPL for command handling and add help/exit commands

--- main.go
+++ main.go
@@ -1,31 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-
-		cleanedInput := cleanInput(input)
-
-		if len(cleanedInput) > 0 {
-			fmt.Println("Your command was:", cleanedInput[0])
-		}
-	}
-}
-
-func cleanInput(text string) []string {
-	var result []string
-	lower := strings.ToLower(text)
-	trim := strings.TrimSpace(lower)
-	result = strings.Fields(trim)
-	return result
+	startRepl()
 }
